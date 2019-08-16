01d9f9748868ca8a372ecd510d4be93c0c50155b
Ariel <arielfoy2@gmail.com>
2019-08-16 17:33:49 +0300

    index

--- serviceworker.js
+++ serviceworker.js
@@ -4,7 +4,7 @@
     // Update 'version' if you need to refresh the cache
     var version = 'v1.0::CacheFirstSafe';
     var offlineUrl = "./offline.html"; // <-- Offline/Index.cshtml
-    var urlsToCache = [ './', offlineUrl ]; // <-- Add more URLs you would like to cache.
+    var urlsToCache = [ './', offlineUrl, './index.html' ]; // <-- Add more URLs you would like to cache.
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
