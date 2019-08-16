abcde12a980e93c8ac99c0fffb8815cb597e8547
Ariel <arielfoy2@gmail.com>
2019-08-16 18:03:28 +0300

    PWAtest

--- serviceworker.js
+++ serviceworker.js
@@ -2,8 +2,8 @@
     'use strict';
     // Update 'version' if you need to refresh the cache
     var version = 'v1.0::CacheFirstSafe';
-    var offlineUrl = "PWAtest/offline.html"; // <-- Offline/Index.cshtml
-    var urlsToCache = [ 'PWAtest/', offlineUrl, 'PWAtest/index.html' ]; // <-- Add more URLs you would like to cache.
+    var offlineUrl = "/PWAtest/offline.html"; // <-- Offline/Index.cshtml
+    var urlsToCache = [ '/PWAtest/', offlineUrl, '/PWAtest/index.html' ]; // <-- Add more URLs you would like to cache.
 
     // Store core files in a cache (including a page to display when offline)
     function updateStaticCache() {
